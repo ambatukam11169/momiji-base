e292015d6c51ff5d8f7ee8813665d4ddd9d8795d
ambatukam11169 <daffaaditya1691@gmail.com>
2026-03-26 05:36:00 +0800

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ownerNumber: "6285801135297", // Ganti ke nomor WA kamu
+    botNumber: "6285801135297",   // Ganti ke nomor yang ingin jadi Bot Utama
+    name: "momiji bot",
+    prefix: "/",
+    dbPath: "./database.json",
+    cloneDbPath: "./clonebotdb.json"
+};
